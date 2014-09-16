62fecd5a25cc46f9860b718e9588bcd2ab599af4
yysa <guillerhr@gmail.com>
2014-09-16 09:40:37 +0200

    Separated tests for Hext and Constable methods

--- tests/testthat/test-constable.R
+++ tests/testthat/test-constable.R
@@ -1,4 +1,4 @@
-context("AMS analysis methods")
+context("AMS analysis method: Bootstrap")
 
 load('backwards-data.rda')
 
@@ -19,32 +19,6 @@ test_that("Bootstrap method is backwards compatible", {
                   boot_analysis <- ams.constable(measures, setup, 
                                                  alpha = alpha, R = R)
                   expect_that(boot_analysis, equals(get(boot_analysis_name)))
-
-              }
-          }
-})
-
-test_that("Hext method is backwards compatible", {
-          setup <- ams.setup()
-
-          for (i in 1:3) {
-              for (j in 1:4) {
-                  measure_name <- paste('measures', i, '_', j, sep = '')
-                  measures <- get(measure_name)
-
-                  # Hext analysis
-                  set.seed(1000001)
-                  hext_analysis_name <- paste('hext', i, '_', j, sep = '')
-                  hext_analysis <- ams.hext(measures, setup, 
-                                                 alpha = alpha)
-                  expect_that(hext_analysis, equals(get(hext_analysis_name)))
-
-                  # Hext without N analysis
-                  set.seed(1000001)
-                  hextwon_analysis_name <- paste('hextwon', i, '_', j, sep = '')
-                  hextwon_analysis <- ams.hext.woN(measures, setup, 
-                                                 alpha = alpha)
-                  expect_that(hextwon_analysis, equals(get(hextwon_analysis_name)))
 
               }
           }
---
+++ tests/testthat/test-hext.R
@@ -0,0 +1,32 @@
+context("AMS analysis method: Hext")
+
+load('backwards-data.rda')
+
+alpha <- 0.9
+
+test_that("Hext method is backwards compatible", {
+          setup <- ams.setup()
+
+          for (i in 1:3) {
+              for (j in 1:4) {
+                  measure_name <- paste('measures', i, '_', j, sep = '')
+                  measures <- get(measure_name)
+
+                  # Hext analysis
+                  set.seed(1000001)
+                  hext_analysis_name <- paste('hext', i, '_', j, sep = '')
+                  hext_analysis <- ams.hext(measures, setup, 
+                                                 alpha = alpha)
+                  expect_that(hext_analysis, equals(get(hext_analysis_name)))
+
+                  # Hext without N analysis
+                  set.seed(1000001)
+                  hextwon_analysis_name <- paste('hextwon', i, '_', j, sep = '')
+                  hextwon_analysis <- ams.hext.woN(measures, setup, 
+                                                 alpha = alpha)
+                  expect_that(hextwon_analysis, equals(get(hextwon_analysis_name)))
+
+              }
+          }
+})
+
